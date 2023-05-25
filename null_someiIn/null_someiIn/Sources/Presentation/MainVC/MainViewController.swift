39d1a7234459b2ddaead3f59479fb1e98d2475dd
Ju-Eun <104663234+MelBIP@users.noreply.github.com>
2023-05-25 21:00:51 +0900

    TEST: tabBar 테스트 코드

--- null_someiIn/null_someiIn/Sources/Presentation/MainVC/MainViewController.swift
+++ null_someiIn/null_someiIn/Sources/Presentation/MainVC/MainViewController.swift
@@ -2,7 +2,22 @@ import UIKit
 
 class MainViewController: BaseViewController {
     
+    private let amumal = UILabel().then {
+        $0.text = "Main입니당 ^3^"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func addView() {
+        view.addSubview(amumal)
+    }
+    
+    override func setLayout() {
+        amumal.snp.makeConstraints {
+            $0.centerX.equalTo(view)
+            $0.centerY.equalTo(view)
+        }
     }
 }
--- null_someiIn/null_someiIn/Sources/Presentation/RankVC/RankViewController.swift
+++ null_someiIn/null_someiIn/Sources/Presentation/RankVC/RankViewController.swift
@@ -1,7 +1,24 @@
 import UIKit
 
 class RankViewController: BaseViewController {
+    
+    private let amumal = UILabel().then {
+        $0.text = "Rank입니당 ^3^"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func addView() {
+        view.addSubview(amumal)
+    }
+    
+    override func setLayout() {
+        amumal.snp.makeConstraints {
+            $0.centerX.equalTo(view)
+            $0.centerY.equalTo(view)
+        }
+    }
+    
 }
