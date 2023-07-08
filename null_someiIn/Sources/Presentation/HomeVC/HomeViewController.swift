8b5a25c83c3ab4345987ef56513f00b1bd7b9d6f
MelBIP <s22005@gsm.hs.kr>
2023-07-08 23:20:30 +0900

    ADD: View 위치

--- null_someiIn/Sources/Presentation/HomeVC/HomeViewController.swift
+++ null_someiIn/Sources/Presentation/HomeVC/HomeViewController.swift
@@ -3,7 +3,7 @@ import UIKit
 class HomeViewController: BaseViewController, UITableViewDataSource, UITableViewDelegate {
     
     private let labelView = UIView().then {
-        $0.backgroundColor = .white
+        $0.backgroundColor = .lightGrayCustom
     }
     
     private let someiLabel = UILabel().then {
@@ -27,146 +27,43 @@ class HomeViewController: BaseViewController, UITableViewDataSource, UITableView
         $0.tintColor = .gray
     }
     
-    private let githubstatsView1 = UIView().then {
-        $0.backgroundColor = .lightGrayCustom
-        $0.layer.cornerRadius = 16
+    private let profileView = UIView().then {
+        $0.backgroundColor = .white
     }
     
-    private let profileImage = UIImageView().then {
-        $0.image = UIImage(systemName: "circle.fill")
-        $0.tintColor = .darkGrayCustom
+    private let profileImg = UIImageView().then {
+        $0.image = UIImage(systemName: "person.circle.fill")
+//        $0.foregroundColor = UIColor(bl)
     }
     
-    private let gitstatsLabel = UILabel().then {
-        $0.text = "Github Stats"
-        $0.textColor = .darkGrayCustom
-        $0.font = UIFont(name: "Pretendard-Regular", size: 12)
-    }
-
-    private let userNameLabel = UILabel().then {
-        $0.text = "Test User's"
-        $0.font = UIFont(name: "Pretendard-SemiBold", size: 16)
+    private let nameLabel = UILabel().then {
+        $0.text = "김소마"
+        
     }
     
-    private let statsProgressView = UIProgressView().then {
-        $0.progressTintColor = .blueCustom
-        $0.trackTintColor = .white
-        $0.progress = 0.3
-    }
-
-    private let githubstatsView2 = UIView().then {
-        $0.backgroundColor = .lightGrayCustom
-        $0.layer.cornerRadius = 16
-    }
-    
-    private let starsLabel = UILabel().then {
-        $0.text = "Stars Earned"
-        $0.textColor = .darkGrayCustom
-        $0.font = UIFont(name: "Pretendard-SemiBold", size: 14)
-    }
-    
-    private let starsNumLabel = UILabel().then {
-        $0.text = "0"
-        $0.textColor = .darkGrayCustom
-        $0.font = UIFont(name: "Pretendard-Regular", size: 14)
-    }
-    
-    private let prsLabel = UILabel().then {
-        $0.text = "PRs"
-        $0.textColor = .darkGrayCustom
-        $0.font = UIFont(name: "Pretendard-SemiBold", size: 14)
-    }
-    
-    private let prsNumLabel = UILabel().then {
-        $0.text = "2"
-        $0.textColor = .darkGrayCustom
-        $0.font = UIFont(name: "Pretendard-Regular", size: 14)
-    }
-    
-    private let issuesLabel = UILabel().then {
-        $0.text = "Issues"
-        $0.textColor = .darkGrayCustom
-        $0.font = UIFont(name: "Pretendard-SemiBold", size: 14)
-    }
-    
-    private let issuessNumLabel = UILabel().then {
-        $0.text = "0"
-        $0.textColor = .darkGrayCustom
-        $0.font = UIFont(name: "Pretendard-Regular", size: 14)
-    }
-    
-    private let commitsLabel = UILabel().then {
-        $0.text = "Commits"
-        $0.textColor = .darkGrayCustom
-        $0.font = UIFont(name: "Pretendard-SemiBold", size: 14)
-    }
-    
-    private let commitsNumLabel = UILabel().then {
-        $0.text = "204"
-        $0.textColor = .darkGrayCustom
-        $0.font = UIFont(name: "Pretendard-Regular", size: 14)
-    }
-    
-    private let contriLabel = UILabel().then {
-        $0.text = "Contributed to"
-        $0.textColor = .darkGrayCustom
-        $0.font = UIFont(name: "Pretendard-SemiBold", size: 14)
-    }
-    
-    private let contriNumLabel = UILabel().then {
-        $0.text = "8"
-        $0.textColor = .darkGrayCustom
-        $0.font = UIFont(name: "Pretendard-Regular", size: 14)
-    }
-    
-    private let postView = UIView().then {
-        $0.backgroundColor = .lightGrayCustom
-        $0.layer.cornerRadius = 16
-    }
-    
-    private lazy var postScrollView: UIScrollView = {
-        let scrollView = UIScrollView().then {
-            $0.translatesAutoresizingMaskIntoConstraints = false
-        }
-        return scrollView
-    }()
-
-    
-    private let postLabel = UILabel().then {
-        $0.text = "인기 게시글"
-        $0.font = UIFont(name: "Pretendard-SemiBold", size: 20)
-    }
-    
-    private lazy var tableView: UITableView = {
-        let tableView = UITableView().then {
-            $0.translatesAutoresizingMaskIntoConstraints = false
-        }
-        return tableView
-    }()
-    
-    private let chevronImage = UIImageView().then {
-        $0.image = UIImage(systemName: "chevron.forward")
-        $0.tintColor = .darkGrayCustom
+    private let listView = UIView().then {
+        $0.backgroundColor = .white
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.dataSource = self
-        tableView.delegate = self
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
-        view.addSubview(tableView)
-                
+        self.view.backgroundColor = .lightGrayCustom
+        configure()
         
+//        tableView.dataSource = self
+//        tableView.delegate = self
+//        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+//        view.addSubview(tableView)
     }
     
     private func configure() {
-        tableView.dataSource = self
-        tableView.rowHeight = 100
+//        tableView.dataSource = self
+//        tableView.rowHeight = 100
     }
     
     override func addView() {
-        [labelView, self.alramImage, self.settingImage, githubstatsView1, githubstatsView2, postView].forEach {
+        [labelView, self.alramImage, self.settingImage, self.profileView, self.listView].forEach {
             view.addSubview($0)
         }
         
@@ -174,19 +71,9 @@ class HomeViewController: BaseViewController, UITableViewDataSource, UITableView
             labelView.addSubview($0)
         }
         
-        [profileImage, gitstatsLabel, userNameLabel, statsProgressView].forEach {
-            githubstatsView1.addSubview($0)
+        [profileImg].forEach {
+            profileView.addSubview($0)
         }
-        
-        [starsLabel, starsNumLabel, prsLabel, prsNumLabel, issuesLabel, issuessNumLabel, commitsLabel, commitsNumLabel, contriLabel, contriNumLabel].forEach {
-            githubstatsView2.addSubview($0)
-        }
-        
-        [postLabel, chevronImage, tableView].forEach {
-            postScrollView.addSubview($0)
-        }
-        
-        postView.addSubview(postScrollView)
     }
     
     override func setLayout() {
@@ -220,134 +107,40 @@ class HomeViewController: BaseViewController, UITableViewDataSource, UITableView
             $0.top.equalTo(view).offset(56)
         }
         
-        githubstatsView1.snp.makeConstraints {
-            $0.height.equalTo(88)
-            $0.left.equalTo(view).offset(20)
-            $0.right.equalTo(view).inset(20)
-            $0.top.equalTo(labelView).offset(40)
+        profileView.snp.makeConstraints {
+            $0.top.equalTo(someiLabel.snp.bottom).offset(20)
+            $0.left.equalTo(view).offset(0)
+            $0.right.equalTo(view).offset(0)
+            $0.height.equalTo(240)
         }
         
-        profileImage.snp.makeConstraints {
-            $0.top.equalTo(githubstatsView1).offset(20)
-            $0.bottom.equalTo(githubstatsView1).inset(20)
-            $0.left.equalTo(githubstatsView1).offset(20)
-            $0.width.equalTo(48)
-            $0.height.equalTo(48)
+        profileImg.snp.makeConstraints {
+            $0.height.width.equalTo(48)
+            $0.top.equalTo(profileView).offset(32)
+            $0.left.equalTo(profileView).offset(20)
         }
         
-        gitstatsLabel.snp.makeConstraints {
-            $0.top.equalTo(githubstatsView1).offset(20)
-            $0.left.equalTo(profileImage.snp.right).offset(10)
-            
+        listView.snp.makeConstraints {
+            $0.top.equalTo(profileView.snp.bottom).offset(12)
+            $0.left.equalTo(view).offset(0)
+            $0.right.equalTo(view).offset(0)
+            $0.height.equalTo(417)
         }
         
-        userNameLabel.snp.makeConstraints {
-            $0.top.equalTo(githubstatsView1).offset(36)
-            $0.left.equalTo(profileImage.snp.right).offset(10)
-        }
         
-        statsProgressView.snp.makeConstraints {
-            $0.bottom.equalTo(githubstatsView1).inset(20)
-            $0.left.equalTo(profileImage.snp.right).offset(10)
-            $0.right.equalTo(githubstatsView2).inset(20)
-            $0.height.equalTo(8)
-        }
-        
-        githubstatsView2.snp.makeConstraints {
-            $0.height.equalTo(124)
-            $0.left.equalTo(view).offset(20)
-            $0.right.equalTo(view).inset(20)
-            $0.top.equalTo(githubstatsView1.snp.bottom).offset(12)
-        }
-        
-        starsLabel.snp.makeConstraints {
-            $0.top.equalTo(githubstatsView2).offset(20)
-            $0.left.equalTo(githubstatsView2).offset(20)
-        }
-        
-        starsNumLabel.snp.makeConstraints {
-            $0.top.equalTo(githubstatsView2).offset(20)
-            $0.right.equalTo(githubstatsView2).inset(200)
-        }
-        
-        prsLabel.snp.makeConstraints {
-            $0.centerY.equalTo(githubstatsView2)
-            $0.left.equalTo(githubstatsView2).offset(20)
-        }
-        
-        prsNumLabel.snp.makeConstraints {
-            $0.centerY.equalTo(githubstatsView2)
-            $0.right.equalTo(githubstatsView2).inset(200)
-        }
-        
-        issuesLabel.snp.makeConstraints {
-            $0.bottom.equalTo(githubstatsView2).inset(20)
-            $0.left.equalTo(githubstatsView2).offset(20)
-        }
-        
-        issuessNumLabel.snp.makeConstraints {
-            $0.bottom.equalTo(githubstatsView2).inset(20)
-            $0.right.equalTo(githubstatsView2).inset(200)
-        }
-        
-        commitsLabel.snp.makeConstraints {
-            $0.top.equalTo(githubstatsView2).offset(20)
-            $0.left.equalTo(githubstatsView2).offset(200)
-        }
-        
-        commitsNumLabel.snp.makeConstraints {
-            $0.top.equalTo(githubstatsView2).offset(20)
-            $0.right.equalTo(githubstatsView2).inset(20)
-        }
-        
-        contriLabel.snp.makeConstraints {
-            $0.centerY.equalTo(githubstatsView2)
-            $0.left.equalTo(githubstatsView2).offset(200)
-        }
-        
-        contriNumLabel.snp.makeConstraints {
-            $0.centerY.equalTo(githubstatsView2)
-            $0.right.equalTo(githubstatsView2).inset(20)
-        }
-        
-        postView.snp.makeConstraints {
-            $0.height.equalTo(419)
-            $0.left.equalTo(view).offset(20)
-            $0.right.equalTo(view).inset(20)
-            $0.top.equalTo(githubstatsView2.snp.bottom).offset(12)
-        }
-        
-        postScrollView.snp.makeConstraints {
-            $0.edges.equalTo(postView)
-        }
-        
-        tableView.snp.makeConstraints { make in
-            make.top.equalTo(postLabel.snp.bottom).offset(20)
-            make.left.right.equalTo(postView)
-            make.height.equalTo(419) // 적절한 높이로 변경해주세요
-        }
-        
-        postLabel.snp.makeConstraints {
-            $0.top.equalTo(postView).offset(20)
-            $0.left.equalTo(postView).offset(20)
-        }
-        
-        chevronImage.snp.makeConstraints {
-            $0.top.equalTo(postView).offset(26)
-            $0.right.equalTo(postView.snp.right).inset(20)
-        }
     }
     
     // UITableViewDataSource methods
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // Return the number of rows in your table view
-        return 0
+        return 10
     }
         
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
             
         // Configure the cell
+        cell.textLabel?.text = "Row \(indexPath.row + 1)"
         
         return cell
     }
